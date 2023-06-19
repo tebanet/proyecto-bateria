cfb350b39657c98d15a95ab5035f3fd83a9e78da
Antonio Rondán <kaysera0@icloud.com>
2023-06-19 21:06:30 +0200

    Los sonidos funcionan

---
+++ scripts/drums.js
@@ -0,0 +1,45 @@
+"use strict";
+function crash() {
+	const crash = new Audio("sounds/crash.wav");
+	crash.play();
+}
+
+function hihatClose() {
+	const hihatClose = new Audio("sounds/hihat-close.wav");
+	hihatClose.play();
+}
+
+function hihatOpen() {
+	const hihatOpen = new Audio("sounds/hihat-open.wav");
+	hihatOpen.play();
+}
+
+function kick() {
+	const kick = new Audio("sounds/kick.wav");
+	kick.play();
+}
+
+function ride() {
+	const ride = new Audio("sounds/ride.wav");
+	ride.play();
+}
+
+function snare() {
+	const snare = new Audio("sounds/snare.wav");
+	snare.play();
+}
+
+function tomHigh() {
+	const tomHigh = new Audio("sounds/tom-high.wav");
+	tomHigh.play();
+}
+
+function tomLow() {
+	const tomLow = new Audio("sounds/tom-low.wav");
+	tomLow.play();
+}
+
+function tomMid() {
+	const tomMid = new Audio("sounds/tom-mid.wav");
+	tomMid.play();
+}
